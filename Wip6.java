86c688e89308c1aa1adaf59f8ca89efb6747a5d8
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-08-05 16:21:52 +0530

    Add files via upload

---
+++ Wip6.java
@@ -0,0 +1,9 @@
+
+public class Wip6 {
+		public static void main(String[] args) {
+	double tot=50,secnd=90,trd=2/3,pass;
+	pass=((tot-tot/10)*2)/3;
+	System.out.print("No. of students passed="+pass);
+	}
+	
+}
