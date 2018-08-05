9b37d22aa2fa5b138004ae36f6b2fb5acd796690
KarthikeyanRRaja <40626199+KarthikeyanRRaja@users.noreply.github.com>
2018-08-05 16:21:27 +0530

    Add files via upload

---
+++ wip4.java
@@ -0,0 +1,10 @@
+import java.lang.Math.*;
+public class wip4 {
+	public static void main(String[] args) {
+		double pbir=5,lb=216,l;
+		l=sqrt(lb*4);
+		System.out.println("Length="+l);
+		
+	}
+		
+}
